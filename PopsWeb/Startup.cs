de2d1d5ac3f95de1acae5846e20043f3bd1b8b60
Tomás António Sanches Pinto <tomas.antonio.sp@gmail.com>
2017-03-16 16:00:22 +0000

    Add project files.

---
+++ PopsWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PopsWeb.Startup))]
+namespace PopsWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
